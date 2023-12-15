cb5785dfb90c49b96acf0105734bc612da026632
Hyeo Min Park <hyeominpark@Hyeos-MacBook-Air.local>
2023-12-15 10:59:47 +0900

    react in progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { v4 } from "uuid";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
+    const [todoList, setTodoList] = useState<Todo[]>([]);
+
+    type Todo = {
+        id: string;
+        title: string;
+        content: string;
+        isDone: boolean;
+    };
+
+    const onChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target;
+        if (name === "title") {
+            setTitle(value);
+        } else if (name === "content") {
+            setContent(value);
+        }
+    };
+
+    const onSubmitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const newTodo = {
+            id: v4(),
+            title,
+            content,
+            isDone: false,
+        };
+        console.log(newTodo);
+        setTodoList([...todoList, newTodo]);
+    };
+
+    return (
+        <div>
+            <div className="form-container">
+                <form onSubmit={onSubmitHandler}>
+                    <div>
+                        <label>제목</label>
+                        <input
+                            name="title"
+                            value={title}
+                            onChange={onChangeHandler}
+                            placeholder="제목을 입력하세요."
+                        />
+                    </div>
+                    <div>
+                        <label>내용</label>
+                        <input
+                            name="content"
+                            value={content}
+                            onChange={onChangeHandler}
+                            placeholder="내용을 입력하세요."
+                        />
+                    </div>
+                    <button>추가하기</button>
+                </form>
+            </div>
+            <div className="card-container">
+                <h2>In Progress</h2>
+                {todoList.map((item) => {
+                    return (
+                        <div className="single-card" key={item.id}>
+                            <h2>{item.title}</h2>
+                            <p>{item.content}</p>
+                            <button>{item.isDone ? "취소" : "완료"}</button>
+                            <button>삭제</button>
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    );
 }
 
 export default App;
